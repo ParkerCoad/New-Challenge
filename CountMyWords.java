428ebb30842145e014cdb91e4d2d5018063ec74d
ParkerCoad <parkeme08@gmail.com>
2018-05-17 16:45:28 -0500

    Add files via upload

--- CountMyWords.java
+++ CountMyWords.java
@@ -1,25 +1,25 @@
-/**
- * 
- */
+
 package ManyWords;
 
-
+/*
+ * Was using to man of the libraries to Read in text from the console and output to the output.txt document. 
+ * I have commented out the libraries that are no longer being used.
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+*/
+//import java.io.BufferedWriter;
+//import java.io.FileWriter;
+//import java.io.InputStreamReader;
+//import java.io.PrintWriter;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.regex.Pattern;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -27,45 +27,28 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * @author mendes.parker
- *
+ * @author mendes parker
+ * HEB Assessment
  */
 public class CountMyWords {
 
 	/**
-	 * @param args
-	 * String eq = "|";
-	 * int [] a;
-	 */		
-	static <K,V extends Comparable<? super V>> 
-    List<Entry<K, V>> entriesSortedByValues(Map<K,V> map) {
-
-List<Entry<K,V>> sortedEntries = new ArrayList<Entry<K,V>>(map.entrySet());
-
-Collections.sort(sortedEntries, 
-    new Comparator<Entry<K,V>>() {
-        @Override
-        public int compare(Entry<K,V> e1, Entry<K,V> e2) {
-            return e2.getValue().compareTo(e1.getValue());
-        }
-    }
-);
-
-return sortedEntries;
-}
-	
+	 *String and Map created. The Map contains both a string and an integer that will keep count of the key and values. 
+	 */
 	static void countWords(String fileName, Map<String, Integer> words) throws FileNotFoundException {
 	Scanner scan = new Scanner(new File(fileName));
 	while (scan.hasNext())
 	{
+		
+	//Take out extra punctuation from each line that is read in.
 		String word = scan.next();
 		Integer count = words.get(word);
 		word = word.replaceAll("\\,", "");
 		word = word.replaceAll("\\.", "");
 		word = word.replaceAll("\\?", "");
 		word = word.replaceAll("\\!", "");
-		// (" ,!?.", "")
 		
+	// counts the words as long as there are words left to count.	
 		if (count != null)
 			count++;
 		else
@@ -74,13 +57,33 @@ return sortedEntries;
 	}
 	scan.close();
 	}
-	@SuppressWarnings("rawtypes")
-	public static void main(String[] args) throws FileNotFoundException{
-		// TODO Auto-generated method stub
-		 
+	
+	/* Sort the values in descending order using compare method, then returns the values. 
+	 * This is meant to organize and sort the values of the output in descending order. 
+	*/
+	static <K,V extends Comparable<? super V>>
+	List<Entry<K, V>> entriesSortedByValues(Map<K,V> map) 
+	{
+		List<Entry<K,V>> sortedEntries = new ArrayList<Entry<K,V>>(map.entrySet());
 		
+		Collections.sort(sortedEntries, new Comparator<Entry<K,V>>() 
+		{
+			@Override
+			public int compare(Entry<K,V> e1, Entry<K,V> e2) 
+			{
+				return e2.getValue().compareTo(e1.getValue());
+			}
+		  }
+		);
+	
+	return sortedEntries;
+	}
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) throws FileNotFoundException, IOException
+	{
+	  // The TreeMap is being used here to contain and order the keys and values of the words that were read in.
 		TreeMap<String, Integer> words = new TreeMap<String, Integer>();
-		countWords("C:\\Users\\mendes.parker\\sample.txt", words);
+		countWords("C:\\Users\\mendes.parker\\input.txt", words);
 		
 		 /* Display content using Iterator*/
 	      Set s = words.entrySet();
@@ -88,69 +91,33 @@ return sortedEntries;
 	      while(iterator.hasNext()) 
 	      {
 	         Map.Entry mapentry = (Map.Entry)iterator.next();
-	         //int j = mapentry; 
-	         
+	        // Prints out the keys and adds a pipe. Plan to have the pipe delimiter be the middle of the sort.	         
 	         System.out.print(mapentry.getKey() + "|") ;
+	        /* Unfortunately I was not able to have the "=" print equally as the word values incremented, but 
+	         * that is the effort below. 
+	         */
 	         for (int eq = 0; eq <= 3 ; eq++)
 	         System.out.print("=");
-	         //System.out.println(" = " + "(" + mapentry.getValue() + ")");
 	         System.out.println( "(" + mapentry.getValue() + ")");
-	        
-
-	 		/*while( s!= mapentry) {
-	 			System.out.print("=");
-	 		} */
-	 			
-	      }
-	           
-	        
+	        }
+	      
 	      try{	
-	    	  //Reassigns the standard output
+	    	  /*Reassigns the standard console data to output and places it in the correct location. 
+	    	   * The file is being created, but the console output is not writting to the file. 
+	    	  */
 	    	     PrintStream out = new PrintStream(new FileOutputStream("C:\\Users\\mendes.parker\\output.txt"));
 		         System.setOut(out);
-		         out.close();		       
-		         
-		       /*Used to connect to the console. read lines of output from the console.
-		        * 
-		        */
-		         
-		       //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		       
-		       /*Used to read lines of output from the console.
-		        * 
-		        */
-		       //String lines = br.readLine();
-		       
-		       //Used to write to a file.
-		       //PrintWriter outp = new PrintWriter(new FileWriter("C:\\Users\\mendes.parker\\output.txt"));
-		       //File f1 = new File("C:\\Users\\mendes.parker\\output.txt");
-		       //if(!f1.exists()){
-		    	 //  f1.createNewFile();
-		       //}
-		       //new
-		       /*
-		       FileWriter fw = new FileWriter (f1.getName(),true);
-		       BufferedWriter bw = new BufferedWriter(fw);
-		       bw.write(lines);
-		       bw.close();
-		       
-		       outp.println(lines);
-		       
-		       */
-		       //PrintStream out = new PrintStream(new FileOutputStream("C:\\Users\\mendes.parker\\output.txt"));
-		       //System.setOut(out);
-		       //out.close();
-		       
-		       //Close File
-		       //outp.close();
-		       		       
-	      }
-	      //If there is a problem with the output being created. The user will receive an error message.
+		         out.close();	
+	         }
+	      /*If there is a problem with the output being created. The user will receive an error message.
+	       * There are more checks to do to avoid confusion later. i.e. NullPointerException, ParseException and 
+	       * FileNotFoundException.
+	      */
 	      catch(IOException e)
 	      {
 	    	  e.printStackTrace();
 	    	  System.out.println("There is an error with creating and saving output.txt.");
 	      }
-	      }
-		}
-    
+	      
+         }
+     }
